625b015449bac312b633aae7fc2160ac30eedeac
LaurenForster25 <lauren.forster2002@gmail.com>
2025-12-06 05:59:18 +0000

    Adding access to book fair spreadsheet

--- run.py
+++ run.py
@@ -10,3 +10,4 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('ss.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('book-fair')
